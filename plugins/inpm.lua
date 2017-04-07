825d2f6b8ad1b228db8e0130d8402a642abfc80d
mmosavi <mosavimohamad80@gmail.com>
2017-04-07 10:41:07 -0700

    Update inpm.lua

--- plugins/inpm.lua
+++ plugins/inpm.lua
@@ -208,7 +208,7 @@ if to == 'user' or service or is_admin1(msg) and to == "chat" or to == "channel"
 
 	if matches[1] == 'help' and msg.to.type == 'user' or matches[1] == 'pmhelp' and is_admin1(msg) and msg.to.type ~= 'user' then
       	savelog(msg.to.id, name_log.." ["..msg.from.id.."] used pm help")
-		text = "به ربات انتی اسپمر SPM خوش آمدید/nبرای ارتباط با صاحب این ربات به آیدی زیر پیام بدهد/n@storent/nبا تشکر از توجه شما"
+		text = "به ربات انتی اسپمر SPM خوش آمدید\nبرای ارتباط با صاحب این ربات به آیدی زیر پیام بدهد\n@storent\nبا تشکر از توجه شما"
      	return text
     end
 
